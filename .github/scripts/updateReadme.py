e304177b354ba0774fa6b494ce538b65a8e42dca
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-13 16:34:09 +0100

    Update updateReadme.py

--- .github/scripts/updateReadme.py
+++ .github/scripts/updateReadme.py
@@ -20,10 +20,11 @@ if __name__ == "__main__":
     with open(readme, 'w', encoding="utf-8") as readme_file:
         repo = data.get("repo")
         introduction = ""
+        maxTestIndex = data.get("maxTestIndex", 0)
         if repo != None or repo != False:
             statusURL = f"https://github.com/{repo}/actions/workflows/classroom.yml"
-            introduction = f"[![GitHub Classroom Workflow]({statusURL}/badge.svg)]({statusURL}) \n\n" # FIXME: Der Link stimmt nicht mehr so ganz ...
-
+            introduction = f"[![GitHub Classroom Workflow]({statusURL}/badge.svg)]({statusURL}) [![Points badge](../../blob/badges/.github/badges/points.svg)](../../actions) \n\n" # FIXME: Der Link stimmt nicht mehr so ganz ...
+            
         textIntro = data.get("introduction")
         introduction += textIntro
         total_points = 0
@@ -40,13 +41,23 @@ if __name__ == "__main__":
             write_intro(readme_file, "Aufgabe", introduction)
             readme_file.write(f"* {total_points} Punkte\n* {timeframe}\n")
             write_horizontal_line(readme_file)
-        
+            hint = "Ladet eure Lösung bitte nicht in den .github-Ordner. "
+            readme_file.write(f"<p>{hint} </p>\n")
+            write_horizontal_line(readme_file)
         readme_file.write(f"<ol>\n")
-        for test in data.get("tests"):
+        tests = data.get("tests")
+        if int(maxTestIndex) > 0:
+            tests = tests[:int(maxTestIndex)]
+            print("Slicing tests", tests)
+        counter = 1
+        for test in tests:
             have_specs = test.get("specs")
             points = test.get("points")
             title = have_specs.get("title")
+            statusURL=f"[![Test Status](../../blob/badges/.github/badges/testStatus_{counter}.svg)](../../actions)"
+            readme_file.write(f"\n{statusURL}  \n")
             readme_file.write(f"<li> {title} ({points} Punkte)</li>\n")
+            counter += 1
             
                 
             if "list" in have_specs:
